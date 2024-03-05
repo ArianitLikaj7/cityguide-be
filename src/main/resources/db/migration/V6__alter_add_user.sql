b6208b1eb0d7d1a5fcb07caf78775a824102f9cf
Arianit Likaj <arianitlikaj46@gmail.com>
2024-03-05 21:08:29 +0100

    fixed a bug in security

---
+++ src/main/resources/db/migration/V6__alter_add_user.sql
@@ -0,0 +1,8 @@
+insert into users(id, created_at, first_name, last_name, username, password, role)
+values (2,
+        current_timestamp,
+        'Arianit',
+        'Likaj',
+        'arianit',
+        '$2a$10$Cz9xGfJtHLAPEZIHsHQat.udW9TdYwHKlqP9OZT9dSpiThlzGN2dS',
+        'ADMIN');
