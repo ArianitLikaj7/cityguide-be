2c0beb1192636e9128a629f2481f48b89fbf377f
Arianit Likaj <arianitlikaj46@gmail.com>
2024-04-25 23:23:01 +0200

    fixed some bugs

---
+++ src/main/java/com/arianit/cityguidebe/exception/MismatchedInputException.java
@@ -0,0 +1,7 @@
+package com.arianit.cityguidebe.exception;
+
+public class MismatchedInputException extends RuntimeException{
+    public MismatchedInputException(String message){
+        super(message);
+    }
+}
